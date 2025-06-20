82bae5ba8ca573555adbea0ca4b641a602867f50
Santosh Rimal <2054santoshrimal@gmail.com>
2025-06-20 22:26:37 +0545

    remove unused route for frontend index page

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,6 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::get('/index',function(){
-return Inertia::render('frontend/index');
-})->name('index');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
